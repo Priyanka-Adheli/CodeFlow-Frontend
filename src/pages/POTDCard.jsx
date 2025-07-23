699697d387eab334cb9c662c47b7d8adffd512f4
Priyanka-Adheli <priyankaadheli12@gmail.com>
2025-07-23 19:56:05 +0530

    Update POTDCard.jsx added theme version

--- src/pages/POTDCard.jsx
+++ src/pages/POTDCard.jsx
@@ -24,17 +24,17 @@ const PotdCard = () => {
   if (error || !potd) return null;
 
   return (
-    <div className="card bg-white border border-slate-200 shadow-md rounded-xl p-4 sm:p-6 mb-6 transition">
+    <div className="card bg-white border border-slate-200 dark:bg-gray-800/90 dark:border-gray-700 shadow-md rounded-xl p-4 sm:p-6 mb-6 transition">
       <div className="flex items-center gap-3">
-        <FaRegCalendar className="text-indigo-950 text-xl" />
+        <FaRegCalendar className="text-indigo-800 text-xl dark:text-white" />
         <div>
           <h3
             onClick={() => navigate(`/problem/${potd._id}`)}
-            className="text-xl font-semibold text-indigo-700 hover:underline cursor-pointer transition"
+            className="text-xl font-semibold text-indigo-700 hover:underline cursor-pointer transition dark:text-white"
           >
             {potd.title}
           </h3>
-          <p className="text-sm text-indigo-600">
+          <p className="text-sm text-indigo-600 dark:text-gray-200">
             Practice today's hand-picked challenge!
           </p>
         </div>
